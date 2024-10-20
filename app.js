67f97a294ab1e9407e99998b65510bad5a4375bf
liqu1di <liqu1dii@mail.ru>
2024-10-21 01:01:10 +0300

    Commit deleted conflict

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#FFD1DC', '#7FB5B5', '#FCE883', 'E6E6FA', '#C6DF90', '#AFDAFC', '#EAE0C8', '#E4717A', '#A8E4A0', '#FFC1CC']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
